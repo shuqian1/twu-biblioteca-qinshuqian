643340a8748d9d7a91182729a114c30af736dfae
qinshuqian <524477767@qq.com>
2019-08-09 16:56:53 +0800

    addWelcomeMessageTest

---
+++ src/com/twu/biblioteca/model/Book.java
@@ -0,0 +1,5 @@
+package com.twu.biblioteca.model;
+
+public class Book {
+
+}
---
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class BibliotecaTest {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    final PrintStream printStream = System.out;
+
+    @Before
+    public void  setUpStream(){
+        System.setOut((new PrintStream(outContent)));
+    }
+
+    @After
+    public void restoreStream(){
+        System.setOut(printStream);
+    }
+
+    @Test
+    public void shouldPrintWelcomeMessage(){
+        assertThat("",equalTo("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore"));
+    }
+}
